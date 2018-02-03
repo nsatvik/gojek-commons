647d61da0621ffdd97fbbd602b1a61244126803c
Satvik <nsatvik@gmail.com>
2018-02-03 10:45:53 +0530

    Jedis connection config

--- gojek-commons-cache/src/test/java/com/gojek/cache/redis/JedisConnectionTest.java
+++ gojek-commons-cache/src/test/java/com/gojek/cache/redis/JedisConnectionTest.java
@@ -39,7 +39,7 @@ public class JedisConnectionTest {
     
     @BeforeMethod
     public void setup() {
-        this.configuration = new CacheConfiguration("localhost", 6379, 5, 2, 2, 2);
+        this.configuration = new CacheConfiguration("localhost", 6379, 50, 2, 2, 2);
         connection = new JedisConnection();
     }
 
